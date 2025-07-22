5faf59f6f5926e0914497f7f45bd47564831521f
Mahdi <mahdiasd96@yahoo.com>
2025-07-22 09:49:59 +0330

    feat: Add flag video feature
    This commit introduces the ability for users to flag videos.
    
    Key changes:
    - Added a flag icon to the video detail page.
    - Implemented the `OnFlagClick` event in `VideoDetailBloc` to handle flag actions.
    - Updated `WebSidebar` layout and animation duration.
    - Added `chewie` and `video_player` dependencies for enhanced video playback.
    - Refactored DI modules to include new API services and use cases.
    - Modified initial route to `LibraryRoute`.
    - Added mapping for `VideoResponse` to `Video` and `FileResponse` to `PhysioFile` in the data layer.
    
    Took 1 hour 39 minutes

--- packages/domain/lib/src/di/domain_di.module.dart
+++ packages/domain/lib/src/di/domain_di.module.dart
@@ -9,10 +9,16 @@ import 'dart:async' as _i687;
 import 'package:domain/domain.dart' as _i494;
 import 'package:domain/src/repository/user/user_repository.dart' as _i498;
 import 'package:domain/src/usecase/config/get_config_use_case.dart' as _i927;
+import 'package:domain/src/usecase/library/get_library_use_case.dart' as _i242;
+import 'package:domain/src/usecase/storage/read_access_token_use_case.dart'
+    as _i553;
 import 'package:domain/src/usecase/user/login_use_case.dart' as _i203;
 import 'package:domain/src/usecase/user/register_use_case.dart' as _i531;
+import 'package:domain/src/usecase/user/resend_otp_use_case.dart' as _i796;
+import 'package:domain/src/usecase/user/reset_password_use_case.dart' as _i491;
 import 'package:domain/src/usecase/user/send_otp_codes_use_case.dart' as _i979;
 import 'package:domain/src/usecase/user/validate_email_use_case.dart' as _i706;
+import 'package:domain/src/usecase/video/get_videos_use_case.dart' as _i691;
 import 'package:injectable/injectable.dart' as _i526;
 
 class DomainPackageModule extends _i526.MicroPackageModule {
@@ -21,6 +27,8 @@ class DomainPackageModule extends _i526.MicroPackageModule {
   _i687.FutureOr<void> init(_i526.GetItHelper gh) {
     gh.factory<_i927.GetConfigUseCase>(
         () => _i927.GetConfigUseCase(gh<_i494.ConfigRepository>()));
+    gh.factory<_i691.GetVideosUseCase>(
+        () => _i691.GetVideosUseCase(gh<_i494.VideoRepository>()));
     gh.factory<_i203.LoginUseCase>(
         () => _i203.LoginUseCase(gh<_i498.UserRepository>()));
     gh.factory<_i531.RegisterUseCase>(
@@ -29,5 +37,13 @@ class DomainPackageModule extends _i526.MicroPackageModule {
         () => _i979.VerifyEmailUseCase(gh<_i498.UserRepository>()));
     gh.factory<_i706.ValidateEmailUseCase>(
         () => _i706.ValidateEmailUseCase(gh<_i498.UserRepository>()));
+    gh.factory<_i796.ResendOTPUseCase>(
+        () => _i796.ResendOTPUseCase(gh<_i498.UserRepository>()));
+    gh.factory<_i491.ResetPasswordUseCase>(
+        () => _i491.ResetPasswordUseCase(gh<_i498.UserRepository>()));
+    gh.factory<_i242.GetLibraryUseCase>(
+        () => _i242.GetLibraryUseCase(gh<_i494.VideoLibraryRepository>()));
+    gh.factory<_i553.ReadAccessTokenUseCase>(
+        () => _i553.ReadAccessTokenUseCase(gh<_i494.StorageRepository>()));
   }
 }
